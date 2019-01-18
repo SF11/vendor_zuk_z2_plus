d4b7cbfaa3f4e2405282753434383ce7ba890e41
dianlujitao <dianlujitao@lineageos.org>
2019-01-18 22:12:20 +0000

    msm8996: Kang ims.apk and qti-telephony-common.jar from daisy

--- Android.mk
+++ Android.mk
@@ -132,7 +132,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ims
 LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/app/ims/ims.apk
+LOCAL_SRC_FILES := proprietary/priv-app/ims/ims.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
